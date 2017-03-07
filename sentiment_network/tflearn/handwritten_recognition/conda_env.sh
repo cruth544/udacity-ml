be53270053cec85f02b238dfa15b5fb3b681a930
Chad Rutherford <c.ruth544@gmail.com>
2017-03-07 09:22:10 -0800

    sync

---
+++ sentiment_network/tflearn/handwritten_recognition/conda_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "source activate handwritten_recognition"
+source activate handwritten_recognition
---
+++ tensorflow/conda_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "source activate tensorflow"
+source activate tensorflow
+
